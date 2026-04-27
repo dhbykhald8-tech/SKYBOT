7434b7a31bbaa24e1c2ce7777faa659e171f4d1a
dhbykhald8-tech <dhbykhald8@gmail.com>
2026-04-27 07:14:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import discord
-from discord.ext import commands
-
-
-TOKEN = 'MTQ5ODEzOTEyODAzNzExMzg1Ng.GmvzJ_.7Y9NZW84CUR2q2GKIZme_4gZQbKDjzXH33Udo'
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
+client = discord.Client(intents=intents)
+
+@client.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f'Logged in as {client.user}')
 
-@bot.command()
-async def hi(ctx):
-    await ctx.send('Hello! Sky Bot is Online 🚀')
+token = os.getenv('DISCORD_TOKEN')
 
-bot.run(TOKEN)
+if token:
+    client.run(token)
+else:
+    print("خطأ: لم يتم العثور على التوكن في إعدادات Railway!")
